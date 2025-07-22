aa24ee80349b715403ef0f998abff12d6d3efbbf
deepthiboroyal <deepthiboroyal2@gmail.com>
2025-07-22 16:12:40 +0530

    strong test for shirt

--- tshirts.cpp
+++ tshirts.cpp
@@ -15,7 +15,9 @@ char size(int cms) {
 
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
+    assert(size(5) == 'error');
     assert(size(37) == 'S');
+    assert(size(38) == 'M'); //asserting that customer prefers relaxed fit
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
